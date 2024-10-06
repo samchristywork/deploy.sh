0ada81bcfa3ea3c413e30295abb3a48d7203fb7c
samchristywork <106397573+samchristywork@users.noreply.github.com>
2024-10-05 21:39:30 -0500

    feat: Add deployment script
    This script deploys an application to a server by uploading files and
    running a few commands.

---
+++ deploy.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+if [ "$#" -ne 3 ]; then
+  echo "Usage: $0 <username> <host> <directory>"
+  exit 1
+fi
+
+USERNAME="$1"
+HOST="$2"
+DIRECTORY="$3"
+
+ssh "$USERNAME@$HOST" "mkdir -p .deploy/ && cd .deploy && rm -rf '$DIRECTORY'" && \
+  scp -r "$DIRECTORY" "$USERNAME@$HOST:.deploy/" && \
+  ssh -f "$USERNAME@$HOST" "cd '.deploy/$DIRECTORY' && nohup ./run.sh < /dev/null > nohup.out 2>&1 &"
