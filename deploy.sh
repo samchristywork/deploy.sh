0ffc7c1731c5b2990dafab72d623c63561b30021
samchristywork <106397573+samchristywork@users.noreply.github.com>
2024-10-05 21:52:29 -0500

    chore: Ensure commands are logged to stdout
    The user can now see which commands are executing, and the script will
    terminate if any command fails.

--- deploy.sh
+++ deploy.sh
@@ -9,6 +9,8 @@ USERNAME="$1"
 HOST="$2"
 DIRECTORY="$3"
 
+set -xe
+
 ssh "$USERNAME@$HOST" "mkdir -p .deploy/ && cd .deploy && rm -rf '$DIRECTORY'" && \
   scp -r "$DIRECTORY" "$USERNAME@$HOST:.deploy/" && \
   ssh -f "$USERNAME@$HOST" "cd '.deploy/$DIRECTORY' && nohup ./run.sh < /dev/null > nohup.out 2>&1 &"
